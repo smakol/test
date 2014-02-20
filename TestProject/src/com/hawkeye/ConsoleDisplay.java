7cda673932ebcb47071c495e7ba148dd17afd1e9
smakol <smakol@CLT-SMAKOL.hawkeyeww.com>
2014-02-20 16:15:51 -0500

    update code.

--- TestProject/src/com/hawkeye/ConsoleDisplay.java
+++ TestProject/src/com/hawkeye/ConsoleDisplay.java
@@ -16,6 +16,11 @@ public class ConsoleDisplay
 	public static void main(String [ ] args)
 	{
 		System.out.println("Saurabh Makol");
+		
+		for (int i = 0; i < 5; i++)
+		{
+			System.out.println((i + 1) + ". Testing");
+		}
 	}
 	
 }
