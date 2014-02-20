50e1e108c8061bca42d28d4cfc6e640031931239
smakol <smakol@CLT-SMAKOL.hawkeyeww.com>
2014-02-20 16:12:24 -0500

    Initial release

---
+++ TestProject/src/com/hawkeye/ConsoleDisplay.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.hawkeye;
+
+/**
+ * @author smakol
+ *
+ */
+public class ConsoleDisplay
+{
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String [ ] args)
+	{
+		System.out.println("Saurabh Makol");
+	}
+	
+}
